bd7ea5576c6f36e3739a5d75a80aa1bfd0f77bab
chalunton <chaluntonvipusanapas@gmail.com>
2025-02-20 20:29:45 +0700

    feat:update html and style tailwind on dev2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"], // ระบุว่า Tailwind จะทำงานกับไฟล์ HTML ในโฟลเดอร์นี้
-  theme: {
-    extend: {
-      screens: {
-        'xs': '375px', // เพิ่ม Breakpoint ใหม่ที่ 375px
-      },
-      colors: {
-        primary: "#FF7342", // สีส้ม
-        secondary: "#FFD167", // สีเหลือง
-        third: "#FFFFFF", // สีพื้นหลัง 1
-        forth: "#F9F9F9", // สีพื้นหลัง 2
-        fifth: "#3B3B3B", // สีดำเข้ม
-      },
-      fontFamily: {
-        sans: ["DM Sans", "sans-serif"],
-        montserrat: ["Montserrat", "sans-serif"],
-      },
-      borderRadius: {
-        base: "8px",
-      },
-      padding: {
-        base: "16px",
+    content: ["./*.html"], // ระบุว่า Tailwind จะทำงานกับไฟล์ HTML ในโฟลเดอร์นี้
+    theme: {
+      extend: {
+        colors: {
+          primary: "#4f46e5", // สีหลัก
+          secondary: "#64748b", // สีรอง
+        },
+        borderRadius: {
+          base: "8px",
+        },
+        padding: {
+          base: "16px",
+        },
       },
     },
-  },
-  plugins: [],
-};
+    plugins: [],
+  };
+  
